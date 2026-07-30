40dc60ada87318348fdd099119fa9d40e29ddc04
AbhayJaiswalgit <jabhay983@gmail.com>
2026-07-30 12:03:57 +0530

    dup middleware

--- backend/routes/budgetRoute.js
+++ backend/routes/budgetRoute.js
@@ -11,11 +11,11 @@ const ensureAuthentication = require("../middlewares/ensureAuthentication");
 
 const router = express.Router();
 
-router.post("/set", ensureAuthentication, handleSetBudget);
-router.get("/current-month", ensureAuthentication, handleGetBudgets);
-router.get("/category-average", ensureAuthentication, handleGetCategoryAverage);
-router.post("/check-exceed", ensureAuthentication, handleCheckBudgetExceed);
-router.put("/update/:id", ensureAuthentication, handleUpdateBudget);
-router.delete("/delete/:id", ensureAuthentication, handleDeleteBudget);
+router.post("/set", handleSetBudget);
+router.get("/current-month", handleGetBudgets);
+router.get("/category-average", handleGetCategoryAverage);
+router.post("/check-exceed", handleCheckBudgetExceed);
+router.put("/update/:id", handleUpdateBudget);
+router.delete("/delete/:id", handleDeleteBudget);
 
 module.exports = router;
